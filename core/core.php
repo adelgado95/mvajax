99b9e84963be636018689332c959fa27b6f21c5f
adelgado95 <ajdfdelgado@gmail.com>
2018-04-08 15:40:22 -0600

    Cambiar datos de conexion

--- core/core.php
+++ core/core.php
@@ -4,8 +4,8 @@ session_start();
 /*Nucleo aqui se llama a todo*/
 #constantes de la conexion
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'estacion');
+define('DB_PASS', 'cm>%d;q@j%HeQE69');
 define('DB_NAME', 'estacion_monitoreo');
 #Constantes de la app
 define('HTML_DIR','views/app/');
